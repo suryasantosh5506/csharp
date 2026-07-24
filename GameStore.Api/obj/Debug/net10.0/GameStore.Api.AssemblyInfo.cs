b2fd8cb6fce5ca085a0db157a65c34c93de9d1c7
suryasantosh5506 <santoshreddy.ksb@gmail.com>
2026-07-24 19:11:17 +0530

    Integrated Rest API with SQlite and understand about Migrations

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46efff23063037d869948bf64732e213eed76885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4312851d93f001df710174ebb5057991d7ac886b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
