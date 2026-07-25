fc5e93fce70d9818d14dfa3bdb997580e92d3dfc
Santosh Reddy <santoshreddy.ksb@gmail.com>
2026-07-25 12:44:10 +0530

    Added Asynchronous Programmin to achieve Non-Blocking Execution

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7051be8eccd5ffb194720fd6bce08fb868b9b9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbd3df6d7a60f7100b03936f8ac30a38af799d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
